59754c46804128d378f9ac7089776311867cfbea
12okoth <ongorojoseph@gmail.com>
2022-09-07 23:09:12 -0700

    a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,37 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Sucess)
- *
- *     */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
-
-Footer
-
-© 2022 GitHub, Inc.
-
-Footer navigation
-
-Terms
-
-Privacy
-
-Security
-
-
