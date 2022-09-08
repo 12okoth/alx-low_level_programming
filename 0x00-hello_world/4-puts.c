4e9107dfdc0ee27989f576e1496158031c8f919d
12okoth <ongorojoseph@gmail.com>
2022-09-07 22:55:58 -0700

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Sucess)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
+
+Security
+
+
